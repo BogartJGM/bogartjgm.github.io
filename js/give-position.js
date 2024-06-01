78785e2216c4d3f890319b9ac4de8212c95ea6b9
Aurorinae <aurorinae@outlook.com>
2024-06-01 13:51:36 -0600

    s

--- js/give-position.js
+++ js/give-position.js
@@ -1,14 +1,14 @@
 /**
- * 
- * @param {HTMLDivElement} containterElement 
+ *
+ * @param {HTMLDivElement} containterElement
  */
 export function givePosition(containterElement) {
-    const containterElementChildren = containterElement.children;
-    Array.from(containterElementChildren).forEach((product, index) => {
-        const spanPosition = product.querySelector("span.posicion");
+  const containterElementChildren = containterElement.children;
+  Array.from(containterElementChildren).forEach((product, index) => {
+    const spanPosition = product.querySelector("span.posicion");
 
-        if (spanPosition) {
-            spanPosition.innerHTML = String(index + 1);
-        }
-    });
+    if (spanPosition) {
+      spanPosition.innerHTML = String(index + 1);
+    }
+  });
 }
